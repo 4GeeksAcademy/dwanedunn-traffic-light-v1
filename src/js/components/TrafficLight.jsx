4e4e8b1e41919b9f1080a62f1d8e5ded3207c020
Dwane Dunn <dwane.dunn@gmail.com>
2025-04-29 00:37:37 +0000

    style traffic lights

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -1,10 +1,10 @@
 const TrafficLight = () => {
   return (
     <>
-      <div className="light-container bg-dark d-flex flex-column justify-content-evenly align-items-center pb-4">
+      <div className="light-container bg-dark rounded d-flex flex-column justify-content-evenly align-items-center pb-4">
         // <div className="light rounded-circle light-r"></div>
         // <div className="light rounded-circle light-y"></div>
-        // <div className="light rounded-circle light-g"></div>
+        // <div className="light rounded-circle light-p"></div>
       </div>
     </>
   );
