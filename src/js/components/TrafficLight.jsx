29b70ee389d4d46d5cbee70f61ee9f3d47970c6b
Dwane Dunn <dwane.dunn@gmail.com>
2025-04-29 00:02:17 +0000

    scafold traffic light component & basic styles

---
+++ src/js/components/TrafficLight.jsx
@@ -0,0 +1,13 @@
+const TrafficLight = () => {
+  return (
+    <>
+      <div className="light-container bg-dark d-flex flex-column p-2">
+        // <div className="light rounded-circle light-r"></div>
+        // <div className="light rounded-circle light-y"></div>
+        // <div className="light rounded-circle light-g"></div>
+      </div>
+    </>
+  );
+};
+
+export default TrafficLight;
