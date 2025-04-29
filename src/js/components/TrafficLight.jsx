73f87ddf44db5b5eabf210b9f1e1a14517a1cfd0
Dwane Dunn <dwane.dunn@gmail.com>
2025-04-29 00:15:01 +0000

    justify content on light container

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -1,7 +1,7 @@
 const TrafficLight = () => {
   return (
     <>
-      <div className="light-container bg-dark d-flex flex-column p-2">
+      <div className="light-container bg-dark d-flex flex-column justify-content-around">
         // <div className="light rounded-circle light-r"></div>
         // <div className="light rounded-circle light-y"></div>
         // <div className="light rounded-circle light-g"></div>
