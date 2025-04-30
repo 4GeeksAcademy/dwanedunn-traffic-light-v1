20b90efa79b84e9de3b8b49e815ef1643579dbb6
Dwane Dunn <dwane.dunn@gmail.com>
2025-04-30 14:48:56 +0000

    style light to match design

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
+
 const TrafficLight = () => {
+  const [color, setColor] = useState("red");
+
   return (
-    <>
+    <div className="wrapper d-flex flex-column justify-content-start align-items-center">
+      <div className="hanger bg-dark"></div>
       <div className="light-container bg-dark rounded d-flex flex-column justify-content-evenly align-items-center pb-4">
         // <div className="light rounded-circle light-r"></div>
         // <div className="light rounded-circle light-y"></div>
-        // <div className="light rounded-circle light-p"></div>
+        // <div className="light rounded-circle light-g"></div>
       </div>
-    </>
+    </div>
   );
 };
 
