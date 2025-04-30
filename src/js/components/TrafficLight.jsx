e15e27d52bec51add96fcf2b931bdd5a1c996177
Dwane Dunn <dwane.dunn@gmail.com>
2025-04-30 18:28:42 +0000

    refactor css

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -4,15 +4,32 @@ const TrafficLight = () => {
   const [state, setState] = useState("");
 
   return (
-    <div className="wrapper d-flex flex-column justify-content-start align-items-center">
-      <div className="hanger bg-dark"></div>
-      <div className="light-container bg-dark rounded d-flex flex-column justify-content-evenly align-items-center pb-4">
-        <div className="light rounded-circle red"></div>
-        <div className="light rounded-circle yellow"></div>
+    <>
+      <div className="wrapper d-flex flex-column justify-content-start align-items-center">
+        <div className="hanger bg-dark"></div>
+        <div className="light-container bg-dark rounded d-flex flex-column justify-content-evenly align-items-center pb-4">
+          <div
+            className="light red"
+            onClick={() => {
+              setState({ selected: "red" });
+            }}
+          ></div>
+          <div
+            className="light yellow"
+            onClick={() => {
+              setState({ selected: "yellow" });
+            }}
+          ></div>
 
-        <div className="light rounded-circle green"></div>
+          <div
+            className="light green"
+            onClick={() => {
+              setState({ selected: "green" });
+            }}
+          ></div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
