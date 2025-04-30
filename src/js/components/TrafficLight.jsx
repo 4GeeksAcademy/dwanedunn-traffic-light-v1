353158e60bf3b68611594202e83aaaa5752e8f1b
Dwane Dunn <dwane.dunn@gmail.com>
2025-04-30 17:16:50 +0000

    refactor css classes for colors

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -7,9 +7,9 @@ const TrafficLight = () => {
     <div className="wrapper d-flex flex-column justify-content-start align-items-center">
       <div className="hanger bg-dark"></div>
       <div className="light-container bg-dark rounded d-flex flex-column justify-content-evenly align-items-center pb-4">
-        // <div className="light rounded-circle light-r"></div>
-        // <div className="light rounded-circle light-y"></div>
-        // <div className="light rounded-circle light-g"></div>
+        // <div className="light rounded-circle red"></div>
+        // <div className="light rounded-circle yellow"></div>
+        // <div className="light rounded-circle green"></div>
       </div>
     </div>
   );
