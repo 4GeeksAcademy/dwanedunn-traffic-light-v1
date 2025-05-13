ca3120ba1ae69baaaf444d9cd1a096efd501a674
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-13 16:52:09 +0000

    refactor lights with css and click handlers

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -13,23 +13,19 @@ const TrafficLight = () => {
         <div className="hanger bg-dark"></div>
         <div className="light-container bg-dark rounded d-flex flex-column justify-content-evenly align-items-center pb-4">
           <div
-            className="light red"
-            onClick={() => {
-              setColor('red');
-            }}
+            className={`light red ${selectedColor === 'red' ? 'glow' : ''}`}
+            onClick={() => handleClick('red')}
           ></div>
           <div
-            className="light yellow"
-            onClick={() => {
-              setColor('Yellow');
-            }}
+            className={`light yellow ${
+              selectedColor === 'yellow' ? 'glow' : ''
+            }`}
+            onClick={() => handleClick('yellow')}
           ></div>
 
           <div
-            className="light green"
-            onClick={() => {
-              setColor('green');
-            }}
+            className={`light green ${selectedColor === 'green' ? 'glow' : ''}`}
+            onClick={() => handleClick('green')}
           ></div>
         </div>
       </div>
