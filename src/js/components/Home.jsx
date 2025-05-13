7f804e5bc7d31e2429cd7acb64204529138348a7
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-13 17:02:35 +0000

    remove rigo image

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,15 +1,12 @@
-import React from "react";
+import React from 'react';
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-import TrafficLight from "./TrafficLight";
+
+import TrafficLight from './TrafficLight';
 
 //create your first component
 const Home = () => {
-  return (
-    <TrafficLight />
-
-  );
+  return <TrafficLight />;
 };
 
 export default Home;
