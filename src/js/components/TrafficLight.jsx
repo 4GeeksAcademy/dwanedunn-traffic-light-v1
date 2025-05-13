b31bd4773681d824ac92b4e553e19404bbcbe639
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-13 17:13:06 +0000

    refactor classnames to use glow

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -13,20 +13,18 @@ const TrafficLight = () => {
         <div className="hanger bg-dark"></div>
         <div className="light-container bg-dark rounded d-flex flex-column justify-content-evenly align-items-center pb-4">
           <div
-            className={`light red ${selectedColor === 'red' ? 'selected' : ''}`}
+            className={`light red ${selectedColor === 'red' ? 'glow' : ''}`}
             onClick={() => handleClick('red')}
           ></div>
           <div
             className={`light yellow ${
-              selectedColor === 'yellow' ? 'selected' : ''
+              selectedColor === 'yellow' ? 'glow' : ''
             }`}
             onClick={() => handleClick('yellow')}
           ></div>
 
           <div
-            className={`light green ${
-              selectedColor === 'green' ? 'selected' : ''
-            }`}
+            className={`light green ${selectedColor === 'green' ? 'glow' : ''}`}
             onClick={() => handleClick('green')}
           ></div>
         </div>
